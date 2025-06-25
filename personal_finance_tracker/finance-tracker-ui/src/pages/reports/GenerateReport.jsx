c10f7932d80f3f69d51f2e67cfd89ce4b287261f
SHEK AHAMAD SIMSARUL HAQ <96659972+Simsar7@users.noreply.github.com>
2025-06-25 21:25:29 +0530

    Update GenerateReport.jsx

--- personal_finance_tracker/finance-tracker-ui/src/pages/reports/GenerateReport.jsx
+++ personal_finance_tracker/finance-tracker-ui/src/pages/reports/GenerateReport.jsx
@@ -14,7 +14,8 @@ const categoryOptions = [
   { value: "lend", label: "Lend" }
 ];
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API;
+
 
 const GenerateReports = () => {
   const [title, setTitle] = useState("");
